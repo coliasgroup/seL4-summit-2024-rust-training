95416c1cc3adecf747b0bc07bc61b41ab15546a2
Nick Spinale <nick@nickspinale.com>
2024-04-24 21:48:36 +0000

    Rework preprocessor

--- book/preprocessor/src/main.rs
+++ book/preprocessor/src/main.rs
@@ -13,17 +13,18 @@ use semver::{Version, VersionReq};
 use std::env;
 use std::fmt::Write;
 use std::io;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 fn main() {
     let matches = Command::new("")
         .subcommand(Command::new("supports").arg(Arg::new("renderer").required(true)))
         .get_matches();
 
-    let local_root = env::var("MDBOOK_FRAGMENT_WITH_CONTEXT_LOCAL_ROOT").unwrap();
-    let remote_prefix = env::var("MDBOOK_FRAGMENT_WITH_CONTEXT_REMOTE_PREFIX").unwrap();
-
-    let preprocessor = This::new(local_root, remote_prefix);
+    let preprocessor = This {
+        local_root: env::var("MDBOOK_GH_LINKS_LOCAL_ROOT").unwrap().into(),
+        repo: env::var("MDBOOK_GH_LINKS_REPO").unwrap(),
+        rev: env::var("MDBOOK_GH_LINKS_REV").unwrap(),
+    };
 
     if let Some(sub_args) = matches.subcommand_matches("supports") {
         let renderer = sub_args.get_one::<String>("renderer").unwrap();
@@ -57,44 +58,51 @@ fn handle_preprocessing(pre: &dyn Preprocessor) -> Result<(), Error> {
 
 pub struct This {
     local_root: PathBuf,
-    remote_prefix: String,
+    repo: String,
+    rev: String,
 }
 
 impl This {
-    pub fn new(local_root: impl AsRef<Path>, remote_prefix: impl AsRef<str>) -> This {
-        This {
-            local_root: local_root.as_ref().to_owned(),
-            remote_prefix: remote_prefix.as_ref().to_owned(),
-        }
+    fn url(&self, path: &str, is_directory: bool) -> String {
+        format!(
+            "https://github.com/{}/{}/{}/{path}",
+            self.repo,
+            if is_directory { "tree" } else { "blob" },
+            self.rev
+        )
     }
 
     fn render_fragment_with_context(
         &self,
         attrs: &str,
-        hidden: &str,
-        visible_path: &str,
+        hidden_path_part: &str,
+        visible_path_part: &str,
         start: &str,
         end: &str,
     ) -> String {
-        let path = format!("{hidden}{visible_path}");
-        let abs_path = format!("{}", self.local_root.join(&path).display());
-        let url = format!("{}/{path}#L{start}-L{end}", self.remote_prefix);
+        let path = format!("{hidden_path_part}{visible_path_part}");
+        let local_path = self.local_root.join(&path).display().to_string();
+        let url = format!("{}#L{start}-L{end}", self.url(&path, false));
 
         let mut s = String::new();
 
-        writeln!(&mut s, "<div class=\"fragment-with-context\">").unwrap();
+        writeln!(&mut s, "<div class=\"fragment-with-gh-link\">").unwrap();
 
-        writeln!(&mut s, "<div class=\"fragment-with-context-context\">").unwrap();
+        writeln!(&mut s, "<div class=\"fragment-with-gh-link-link\">").unwrap();
         write!(&mut s, "<pre><code>").unwrap();
-        write!(&mut s, "<a href=\"{url}\">{visible_path}:{start}:{end}</a>").unwrap();
+        write!(
+            &mut s,
+            "<a href=\"{url}\">{visible_path_part}:{start}:{end}</a>"
+        )
+        .unwrap();
         write!(&mut s, "</code></pre>").unwrap();
         writeln!(&mut s, "").unwrap();
         writeln!(&mut s, "</div>").unwrap();
 
-        writeln!(&mut s, "<div class=\"fragment-with-context-fragment\">").unwrap();
+        writeln!(&mut s, "<div class=\"fragment-with-gh-link-fragment\">").unwrap();
         writeln!(&mut s, "").unwrap();
         writeln!(&mut s, "```{attrs}").unwrap();
-        writeln!(&mut s, "{{{{#include {abs_path}:{start}:{end}}}}}").unwrap();
+        writeln!(&mut s, "{{{{#include {local_path}:{start}:{end}}}}}").unwrap();
         writeln!(&mut s, "```").unwrap();
         writeln!(&mut s, "").unwrap();
         writeln!(&mut s, "</div>").unwrap();
@@ -106,15 +114,16 @@ impl This {
 
     fn render_link(
         &self,
-        hidden: &str,
+        hidden_path_part: &str,
         visible: &str,
-        visible_path: &str,
+        visible_path_part: &str,
         start: Option<&str>,
         end: Option<&str>,
     ) -> String {
-        // HACK take advantage of the fact that .../blob/.../$d redirects to .../tree/.../$d
+        let path = format!("{hidden_path_part}{visible_path_part}");
+        let local_path = self.local_root.join(&path);
         let url = {
-            let mut s = format!("{}/{hidden}{visible_path}", self.remote_prefix);
+            let mut s = self.url(&path, local_path.is_dir());
             if let Some(start) = start {
                 write!(&mut s, "#L{start}").unwrap();
                 if let Some(end) = end {
@@ -123,14 +132,13 @@ impl This {
             }
             s
         };
-
         format!("[{visible}]({url})")
     }
 }
 
 impl Preprocessor for This {
     fn name(&self) -> &str {
-        "fragment-with-context"
+        "gh-links"
     }
 
     fn supports_renderer(&self, renderer: &str) -> bool {
@@ -141,24 +149,24 @@ impl Preprocessor for This {
         book.for_each_mut(|section: &mut BookItem| {
             if let BookItem::Chapter(ref mut ch) = *section {
                 {
-                    let r = Regex::new("\\{\\{\\s*#fragement_with_context\\s+\"(?<attrs>[^}]*)\"\\s+(\\((?<hidden>[^\\)]*)\\))?(?<visible_path>[^}]*):(?<start>\\d+):(?<end>\\d+)\\s*\\}\\}").unwrap();
+                    let r = Regex::new("\\{\\{\\s*#fragment_with_gh_link\\s+\"(?<attrs>[^}]*)\"\\s+(\\((?<hidden_path_part>[^\\)]*)\\))?(?<visible_path_part>[^}]*):(?<start>\\d+):(?<end>\\d+)\\s*\\}\\}").unwrap();
                     ch.content = r.replace_all(&ch.content, |captures: &Captures| {
                         self.render_fragment_with_context(
                             captures.name("attrs").unwrap().as_str(),
-                            captures.name("hidden").map(|m| m.as_str()).unwrap_or(""),
-                            captures.name("visible_path").unwrap().as_str(),
+                            captures.name("hidden_path_part").map(|m| m.as_str()).unwrap_or(""),
+                            captures.name("visible_path_part").unwrap().as_str(),
                             captures.name("start").unwrap().as_str(),
                             captures.name("end").unwrap().as_str(),
                         )
                     }).into_owned();
                 }
                 {
-                    let r = Regex::new("\\{\\{\\s*#link\\s+(\\((?<hidden>[^\\)]*)\\))?(?<visible>(?<visible_path>[^:}]*)(:(?<start>\\d+)(:(?<end>\\d+))?)?)\\s*\\}\\}").unwrap();
+                    let r = Regex::new("\\{\\{\\s*#gh_link\\s+(\\((?<hidden_path_part>[^\\)]*)\\))?(?<visible>(?<visible_path_part>[^:}]*)(:(?<start>\\d+)(:(?<end>\\d+))?)?)\\s*\\}\\}").unwrap();
                     ch.content = r.replace_all(&ch.content, |captures: &Captures| {
                         self.render_link(
-                            captures.name("hidden").map(|m| m.as_str()).unwrap_or(""),
+                            captures.name("hidden_path_part").map(|m| m.as_str()).unwrap_or(""),
                             captures.name("visible").unwrap().as_str(),
-                            captures.name("visible_path").unwrap().as_str(),
+                            captures.name("visible_path_part").unwrap().as_str(),
                             captures.name("start").map(|m| m.as_str()),
                             captures.name("end").map(|m| m.as_str()),
                         )
--- book/src/setup/README.md
+++ book/src/setup/README.md
@@ -6,16 +6,16 @@
 
 # Foo
 
-{{#fragement_with_context "rust,ignore" examples/root-task/hello/src/main.rs:12:17}}
+{{#fragment_with_gh_link "rust,ignore" examples/root-task/hello/src/main.rs:12:17}}
 
-{{#fragement_with_context "rust,ignore" (examples/root-task/)hello/src/main.rs:12:17}}
+{{#fragment_with_gh_link "rust,ignore" (examples/root-task/)hello/src/main.rs:12:17}}
 
-Foo {{#link examples/root-task/hello/src/main.rs:12:17}} bar
+Foo {{#gh_link examples/root-task/hello/src/main.rs:12:17}} bar
 
-Foo {{#link examples/root-task/hello/src/main.rs:12}} bar
+Foo {{#gh_link examples/root-task/hello/src/main.rs:12}} bar
 
-Foo {{#link examples/root-task/hello/src/main.rs}} bar
+Foo {{#gh_link examples/root-task/hello/src/main.rs}} bar
 
-Foo {{#link (examples/root-task/hello/)src/main.rs}} bar
+Foo {{#gh_link (examples/root-task/hello/)src/main.rs}} bar
 
-Foo {{#link examples/root-task/hello/src/}} bar
+Foo {{#gh_link examples/root-task/hello/src/}} bar
