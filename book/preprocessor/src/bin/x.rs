79a21887c9a2f62777f0b020ce1804190777caf4
Nick Spinale <nick@nickspinale.com>
2024-09-22 05:20:46 -0400

    x

--- book/preprocessor/src/bin/x.rs
+++ book/preprocessor/src/bin/x.rs
@@ -6,5 +6,10 @@ fn main() {
     let top = Repository::init(PATH).unwrap();
     let repo = top.find_submodule("code").unwrap().open().unwrap();
     let head = repo.find_commit(repo.refname_to_id("HEAD").unwrap()).unwrap();
-    println!("{:?}", head.message_());
+    let mut commit = head;
+    while commit.summary().unwrap() != "0" {
+        println!("{:?}", commit.summary().unwrap());
+        assert_eq!(commit.parent_count(), 1);
+        commit = commit.parent(0).unwrap();
+    }
 }
